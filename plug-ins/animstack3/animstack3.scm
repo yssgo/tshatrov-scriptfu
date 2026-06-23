15e7d1e7d620ae1c4af99ba9b2612417070be4d0
yssgo <cieltero@gmail.com>
2026-06-24 06:51:01 +0900

    Update animstack3.scm
    coding: utf-8 to encoding: utf-8

--- plug-ins/animstack3/animstack3.scm
+++ plug-ins/animstack3/animstack3.scm
@@ -1,5 +1,5 @@
 #!/usr/bin/env gimp-script-fu-interpreter-3.0
-;;; -*- coding: utf-8 -*-
+;;; -*- encoding: utf-8 -*-
 ;;;!# Close comment started on first line. Needed by gettext.
 
 ;;; +--------------------------------------+
